e6eb6224317e75529ac50fff12d4c5c1630746d3
roxy <roxy@roxy>
2016-01-23 07:26:17 -0430

    actualización de las funciones

--- converter.py
+++ converter.py
@@ -34,37 +34,94 @@ def salir():
 	else:
 		print ("close converter")
 		sys.exit()
-def pon_a_kilopon():
-	salida=(entrada.get()) * 1000
-def kilopon_a_pon():
-	salida=(entrada.get()) / 1000
-def kp_a_n():
-	salida=(entrada.get()) * 9.8
-def n_a_kp():
-	salida=(entrada.get()) / 9.8
-def n_a_dynas():
-	salida=(entrada.get()) * 100000
-def p_a_dynas():
-	salida=(entrada.get()) * 980
-def dynas_a_pon():
-	salida=(entrada.get()) / 980
-def kpadyn():
-	dato=entrada.get()
-	newton=kp_a_n(dato)
-	kp=n_a_dynas(newton)
+
+def Dynas():
+	def n_a_dynas():
+		din=(entrada.get())
+		salida=(din* 100000)
+		respuesta=str(salida)
+		resultado=Label(separador, bg="white",text="Resultado es: ",font=('Helvetica', 12)).grid()
+		resp=Label(separador, bg="lightblue",text=respuesta,font=('Helvetica', 22)).grid()
+	def p_a_dynas():
+		din=(entrada.get())
+		salida=(din * 980)
+		respuesta=str(salida)
+		resultado=Label(separador, bg="white",text="Resultado es: ",font=('Helvetica', 12)).grid()
+		resp=Label(separador, bg="lightblue",text=respuesta,font=('Helvetica', 22)).grid()
+	def dynas_a_pon():
+		din=(entrada.get())
+		salida=(din) / 980
+		respuesta=str(salida)
+		resultado=Label(separador, bg="white",text="Resultado es: ",font=('Helvetica', 12)).grid()
+		resp=Label(separador, bg="lightblue",text=respuesta,font=('Helvetica', 22)).grid()
+	def kpadyn():
+		din=(entrada.get())
+		newton=kp_a_n(din)
+		kp=n_a_dynas(newton)
+		respuesta=str(salida)
+		resultado=Label(separador, bg="white",text="Resultado es: ",font=('Helvetica', 12)).grid()
+		resp=Label(separador, bg="lightblue",text=respuesta,font=('Helvetica', 22)).grid()
+		
+	separador=Tk()
+	separador.geometry("480x340+100+150")
+	separador.title("Dynas")
+	separador.resizable(width=False,height=False)
+	separador.grid()
+	titulo=Label(separador,bg="white",text="Ingresa el Valor Numérico",font=('Helvetica', 12))
+	titulo.grid(padx=50, pady=5)
+	entrada=Entry(separador)
+	entrada.grid(padx=5,pady=5)
+	opciones=Label(separador, bg="white",text="Convertir a: ",font=('Helvetica', 12)).grid()
+	a=Button(separador, bg="white",text="newton a dynas",command=n_a_dynas).grid()
+	b=Button(separador, bg="white",text="pondios a dynas",command=p_a_dynas).grid()
+	c=Button(separador, bg="white",text="dynas a pondios",command=dynas_a_pon).grid()
+	d=Button(separador, bg="white",text="kilopondios a dynas",command=kpadyn).grid()	
+	separador.config(bg="white")
 def Kilopondios():
+	def kp_a_n():
+		kp=float(entrada.get())
+		salida= kp * 9.8
+		respuesta=str(salida)
+		resultado=Label(separador, bg="white",text="Resultado es: ",font=('Helvetica', 12)).grid()
+		resp=Label(separador, bg="lightblue",text=respuesta,font=('Helvetica', 22)).grid()
+	def kilopon_a_pon():
+		kp=float(entrada.get())
+		salida=(kp / 1000)
+		respuesta=str(salida)
+		resultado=Label(separador, bg="white",text="Resultado es: ",font=('Helvetica', 12)).grid()
+		resp=Label(separador, bg="lightblue",text=respuesta,font=('Helvetica', 22)).grid()			
 	separador=Tk()
 	separador.geometry("480x340+100+150")
 	separador.title("Kilopondios")
-	#separador.resizable(width=False,height=False)
+	separador.resizable(width=False,height=False)
 	separador.grid()
 	titulo=Label(separador,bg="white",text="Ingresa el Valor Numérico en Kilopondios",font=('Helvetica', 12))
 	titulo.grid(padx=50, pady=5)
-	entrada=Entry(separador).grid(padx=5,pady=5)
-	a=Button(separador, bg="white",text="a Pondios").grid(row=2, column=0,padx=5,pady=5)
-	b=Button(separador, bg="white",text="a Newton").grid(row=2, column=1,padx=5,pady=5)
+	entrada=Entry(separador)
+	entrada.grid(padx=5,pady=5)
+	opciones=Label(separador, bg="white",text="Convertir a: ",font=('Helvetica', 12)).grid()
+	a=Button(separador, bg="white",text="Pondios",command=kilopon_a_pon).grid()
+	b=Button(separador, bg="white",text="Newton",command=kp_a_n).grid()	
 	separador.config(bg="white")
 	
+def creditos():
+		'''ventana de créditos'''
+		ventana=Toplevel()
+		ventana.title("EDCI Módulo de Ayuda")
+		ventana.geometry("480x480+100+100")
+		ventana.resizable(width=False,height=False)
+		titulo=Label(ventana,fg="blue",bg="lightblue",text="Converter ",font=('Ravie', 24))
+		titulo.pack()
+		titulo=Label(ventana,fg="blue",bg="lightblue",text="programación y diseño por: ",font=('Ravie', 14))
+		titulo.pack()
+		imagenlogo= PhotoImage(file="/home/roxy/proyectos/herramientas/imagenes/python.gif")
+		lab_imagenlogo=Label(ventana,image=imagenlogo,anchor="center",bg="lightblue").pack()
+		titulob=Label(ventana,fg="blue",bg="lightblue",text="Asley B Echarry G",font=('Ravie', 12))
+		titulob.pack()
+		tituloc=Label(ventana,fg="blue",bg="lightblue",text="<asleybach@gmail.com>",font=('Ravie', 12))
+		tituloc.pack()		
+		ventana.config(bg="lightblue")
+		ventana.mainloop()	
 class converter:
 	def __init__(self):
 		formulario=Tk()
@@ -84,11 +141,11 @@ class converter:
 		menu_bar.add_cascade(label="Unidades",menu=unidad, underline=0)
 		menu_bar.add_cascade(label="Ayuda",menu=ayuda, underline=0)
 		unidad.add_command(label="Kilopondios",underline=0,command=Kilopondios)
-		unidad.add_command(label="Dynas",underline=0,)
+		unidad.add_command(label="Dynas",underline=0,command=Dynas)
 		unidad.add_command(label="Newton",underline=0,)
 		unidad.add_command(label="Salir",underline=0,command=salir)
 		ayuda.add_command(label="Acerca de Converter",underline=0,)
-		ayuda.add_command(label="Créditos",underline=0)
+		ayuda.add_command(label="Créditos",underline=0,command=creditos)
 		formulario.protocol("WM_DELETE_WINDOW",salir)
 		formulario.config(bg="lightblue",menu=menu_bar)
 		formulario.mainloop()
